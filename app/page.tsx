e16574b1826683842852d181ff393ea102d4d820
Your Name <you@example.com>
2024-06-18 14:49:48 +0530

    new file:   .env.local 	modified:   .gitignore 	new file:   app/api/auth/[kindeAuth]/route.ts 	modified:   app/globals.css 	modified:   app/page.tsx 	modified:   components/Navbar.tsx 	modified:   package-lock.json 	modified:   package.json 	modified:   tsconfig.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@nextui-org/react";
+import {
+    LoginLink,
+    RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex w-full lg:w-[80%] lg:mx-auto min-h-[calc(100dvh-72px)] flex-col items-center justify-center">
             <h1>Welcome to NextJS + NextUI starter template</h1>
+            <h2>Configured with Kinde Authentication</h2>
+            <div className="flex justify-center items-center gap-6 my-4">
+                <Button className="btn">
+                    <RegisterLink>Register</RegisterLink>
+                </Button>
+                <Button className="btn">
+                    <LoginLink>Login</LoginLink>
+                </Button>
+            </div>
         </main>
     );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center p-4">
-            <h1>Logo</h1>
+            <Link href="/">
+                <h1>Logo</h1>
+            </Link>
             <ThemeSwitcher />
         </nav>
     );
